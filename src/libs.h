553525a52d8bc70109f235d8b754caebc0378275
gray <passer200@163.com>
2020-09-19 15:39:19 +0800

    debayer算法

--- src/libs.h
+++ src/libs.h
@@ -3,6 +3,14 @@
 
 // opencv 
 #include "opencv2\opencv.hpp"
+#ifdef _DEBUG
+#pragma comment(lib, "opencv_world420d.lib")
+#else
+#pragma comment(lib, "opencv_world420.lib")
+#endif // _DEBUG
+
+// cuda
+#include "cuda_runtime.h"
 
 
 #endif // !_LIBS_H_83827384_
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,65 @@
 #include "libs.h"
+#include "cuda\gpu_debayer.h"
 
 using namespace cv;
 
 int main()
 {
     auto path = "F:\\Workdatas\\debayer\\raw.bmp";
-    auto im = imread(path,0);
 
-    Mat rgb(im.rows, im.cols, CV_8UC3);
-    cvtColor(im, rgb, COLOR_BayerBG2BGR);
 
-    imshow("d", rgb);
+    auto raw = imread(path,0);
+    int rows = raw.rows;
+    int cols = raw.cols;
+
+    Mat imcv(rows, cols, CV_8UC3);
+   
+    cvtColor(raw, imcv, COLOR_BayerBG2BGR);
+
+    // 
+    unsigned char *buf_raw_host, *buf_raw_dev;
+    unsigned char *buf_rgb_host, *buf_rgb_host2, *buf_rgb_dev;
+
+    auto curtn = cudaMalloc(&buf_raw_dev, rows * cols);
+    assert(curtn == cudaSuccess);
+
+    curtn = cudaMalloc(&buf_rgb_dev, rows * cols * 3);
+    assert(curtn == cudaSuccess);
+
+    curtn = cudaMallocHost(&buf_rgb_host, rows * cols * 3);
+    assert(curtn == cudaSuccess);
+
+    curtn = cudaMallocHost(&buf_rgb_host2, rows * cols * 3);
+    assert(curtn == cudaSuccess);
+
+    curtn = cudaMemcpy(buf_raw_dev, raw.data, rows * cols, cudaMemcpyHostToDevice);
+    assert(curtn == cudaSuccess);
+
+    gpu_bayer_to_rgb_n3(buf_raw_dev, cols, rows, buf_rgb_dev, nullptr);
+    auto cuerr = cudaGetLastError();
+
+    curtn = cudaMemcpy(buf_rgb_host, buf_rgb_dev, rows * cols * 3, cudaMemcpyDeviceToHost);
+    assert(curtn == cudaSuccess);
+
+    Mat rgb(rows, cols, CV_8UC3, buf_rgb_host);
+    cvtColor(rgb, rgb, COLOR_RGB2BGR);
+
+
+    // old 
+    gpu_bayer_to_rgb(buf_raw_dev, cols, rows, buf_rgb_dev);
+    cuerr = cudaGetLastError();
+
+    curtn = cudaMemcpy(buf_rgb_host, buf_rgb_dev, rows * cols * 3, cudaMemcpyDeviceToHost);
+    assert(curtn == cudaSuccess);
+
+    Mat rgb2(rows, cols, CV_8UC3, buf_rgb_host);
+    cvtColor(rgb2, rgb2, COLOR_RGB2BGR);
+    // end old
+
+    imshow("opencv", imcv);
+    imshow("new", rgb);
+    imshow("old", rgb2);
+
     waitKey();
 
     destroyAllWindows();
